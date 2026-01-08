2ca667cf349d42a610f2f22a92a396936bee717e
Sinchanakkulal <sinchanakulal04@gmail.com>
2026-01-08 11:53:32 +0530

    swapping the two variables

---
+++ Programs/program3.py
@@ -0,0 +1,10 @@
+#write a program to swap the two variables
+ 
+a=input("enter the first variable")
+b=input("enter the second variables")
+print()
+print(f"variables before swapping a={a}, b={b}")
+temp=a
+a=b
+b=temp
+print(f"swapped values a={a}, b={b}")
