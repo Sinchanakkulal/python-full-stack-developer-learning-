eb069317de3e25461c1cfec31b4967023b2fe7c2
Sinchanakkulal <sinchanakulal04@gmail.com>
2026-01-23 22:37:39 +0530

    Programs on the swaping two variables without the use of TEMP variable

---
+++ Programs/program9.py
@@ -0,0 +1,7 @@
+#write a program to swap the two variable without using the temp variable 
+a=input("enter the  first variable a: ")
+b=input("enter the second variable b: ")
+
+a,b=b,a
+print(f"swaped a value is: {a}")
+print(f"swaped b value is: {b}")
