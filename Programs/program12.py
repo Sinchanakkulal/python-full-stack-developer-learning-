81d76a20b994c38666985e435c7cbb56afb13f98
Sinchanakkulal <sinchanakulal04@gmail.com>
2026-02-27 23:27:13 +0530

    adding the 0 at last if it caught in the sequence of number

---
+++ Programs/program12.py
@@ -0,0 +1,56 @@
+"""
+Problem Statement –
+
+A chocolate factory is packing chocolates into the packets. The chocolate packets here represent an array  of N number of integer values. The task is to find the empty packets(0) of chocolate and push it to the end of the conveyor belt(array).
+
+Example 1 :
+
+N=8 and arr = [4,5,0,1,9,0,5,0].
+
+There are 3 empty packets in the given set. These 3 empty packets represented as O should be pushed towards the end of the array
+
+Input :
+
+8  – Value of N
+
+[4,5,0,1,9,0,5,0] – Element of arr[O] to arr[N-1],While input each element is separated by newline
+
+Output:
+
+4 5 1 9 5 0 0 0
+
+Example 2:
+
+Input:
+
+6 — Value of N.
+
+[6,0,1,8,0,2] – Element of arr[0] to arr[N-1], While input each element is separated by newline
+
+Output:
+
+6 1 8 2 0 0
+"""
+#number of chacalates packets 
+N=int(input()) 
+a=[]
+#array of chacalates packets 
+for i in range(0,N):
+    a.append(int(input()))
+
+for index,value in enumerate(a):
+    print(f"index : {index}, value:{value}")
+
+insert_postion =0
+for i in range(0,N):
+    if a[i] != 0:
+        a[insert_postion] = a[i]
+        insert_postion += 1
+
+for i in range(insert_postion,N):
+    a[i] = 0
+
+print(*a,sep=",")
+
+
+
